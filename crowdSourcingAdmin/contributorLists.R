8c3cc1f048f69286ee7b370445b6195e8608dd77
Daniel Pett <dpett@britishmuseum.org>
2015-04-20 10:22:51 +0100

    Few more changes to script

--- crowdSourcingAdmin/contributorLists.R
+++ crowdSourcingAdmin/contributorLists.R
@@ -5,17 +5,17 @@ setwd("~/Documents/research/micropasts/analysis/contributions/") #MacOSX
 #setwd("C:\\micropasts\\analysis") #Windows
 #setwd("micropasts/analysis") #Linux
 
-# Create CSV directory if does not exist
+# Create CSV directory if it does not exist
 if (!file.exists('csv')){
   dir.create('csv')
 }
 
-# Create archives directory if does not exist
+# Create archives directory if it does not exist
 if (!file.exists('archives')){
   dir.create('archives')
 }
 
-# Create JSON folder 
+# Create JSON folder if it does not exist
 if (!file.exists('json')){
   dir.create('json')
 }
@@ -23,15 +23,38 @@ if (!file.exists('json')){
 # Load library
 library(jsonlite)
 
-# Import tasks from json, this method has changed due to coding changes by SciFabric to their code
+# Set the project name
+project <- 'wgs'
 
-download.file("http://crowdsourced.micropasts.org/app/eesAmarna/tasks/export?type=task_run&format=json",'archives/eesAmarnaTasksRun.zip')
-unzip("archives/eesAmarnaTasksRun.zip")
-taskruns <- 'json/eesAmarna_task_run.json'
-file.rename("eesAmarna_task_run.json", taskruns)
+# Set the base url of the application
+baseUrl <- 'http://crowdsourced.micropasts.org/app/'
+
+# Set the task runs api path
+taskruns <- '/tasks/export?type=task_run&format=json'
+
+# Form the export url
+url <- paste(baseUrl,project,taskruns, sep='')
+
+# Create the archive path
+archive <- paste('archives/', project, 'TasksRun.zip', sep='')
+
+# Create the task run file name
+taskruns <- paste(project, '_task_run.json', sep= '' )
+
+# Create the task run file path
+taskrunsPath <- paste('json/', project, '_task_run.json', sep= '' )
+
+# Import tasks from json, this method has changed due to coding changes by SciFabric to their code
+download.file(url, archive)
+
+# Unzip the archive
+unzip(archive)
+
+# Rename the archive
+file.rename(taskruns, taskrunsPath)
 
 # Get the user id from the task run data
-data <- fromJSON(paste(readLines(taskruns), collapse=""))
+data <- fromJSON(paste(readLines(taskrunsPath), collapse=""))
 data <- as.data.frame(data)
 user_id <- data$user_id
 as.data.frame(user_id) -> user_id
@@ -47,7 +70,6 @@ names(userList) <- c("user_id", "fullname")
 
 # Merge the data
 contributors <- merge(user_id, userList, by="user_id")
-
 as.vector(contributors$fullname) -> names
 
 #Extract and print unique names
@@ -55,6 +77,6 @@ unique(names) -> names
 thanks <- paste(as.character(names), collapse=", ")
 
 # Write the thank you list to a text file.
-fileConn<-file("thanksEESArmarna.txt")
+fileConn<-file(paste(project, '.txt', sep=''))
 writeLines(c(thanks), fileConn)
 close(fileConn)
