447e1d0b604e570deff04db39c862c5cc952c08f
Daniel Pett <dpett@britishmuseum.org>
2015-10-16 13:37:01 +0100

    Add r script for petrie
    Uses R markdown as well

---
+++ petrieMuseumDiaries/getData.R
@@ -0,0 +1,123 @@
+#' ----
+#' title: " A script for manipulation of the diaries of Flinders Petrie"
+#' author: "Daniel Pett"
+#' date: "10/16/2015"
+#' output: csv_document
+#' ----
+
+# Set working directory (for example as below)
+setwd("~/Documents/research/micropasts/analysis/petrie/") #MacOSX
+#setwd("C:\\micropasts\\analysis\\petrie") #Windows
+#setwd("micropasts/analysis") #Linux
+
+# Create CSV directory if does not exist
+if (!file.exists('csv')){
+  dir.create('csv')
+}
+
+# Create archives directory if does not exist
+if (!file.exists('archives')){
+  dir.create('archives')
+}
+
+# Create JSON folder 
+if (!file.exists('json')){
+  dir.create('json')
+}
+
+# Add necessary library
+library(jsonlite)
+
+# Load user data
+# http://crowdsourced.micropasts.org/admin/users/export?format=csv (when logged in as admin)
+# This saves as all_users.csv and put this in the csv folder
+
+users <- read.csv('../users/all_users.csv', header=TRUE)
+users <- users[,c("id","fullname","name")]
+
+# Set the project name
+project <- 'Petrie1884'
+# Set the base url of the application
+baseUrl <- 'http://crowdsourced.micropasts.org/project/'
+# Set the task runs api path
+tasks <- '/tasks/export?type=task&format=json'
+# Form the export url
+url <- paste(baseUrl,project, tasks, sep='')
+print(url)
+archives <- paste('archives/',project,'Tasks.zip', sep='')
+print(archives)
+# Import tasks from json, this method has changed due to coding changes by SciFabric to their code
+download.file(url,archives)
+unzip(archives)
+taskPath <- paste('json/', project, '.json', sep='')
+rename <- paste(project, '_task.json', sep='')
+file.rename(rename, taskPath)
+
+# Read json files
+which(lapply(readLines(taskPath), function(x) tryCatch({jsonlite::fromJSON(x); 1}, error=function(e) 0)) == 0)
+trT <- fromJSON(paste(readLines(taskPath), collapse=""))
+trT <- cbind(trT$id,trT$info)
+trTfull <- trT
+
+# extract just task id and image URL, title
+trT <- trT[,c(1,4,6)]
+names(trT) <- c("taskID","imageURL", "imageTitle")
+
+# Import task runs from json
+taskruns <- '/tasks/export?type=task_run&format=json'
+urlRuns <- paste(baseUrl,project, taskruns, sep='')
+print(urlRuns)
+archiveRuns <-paste('archives/', project, 'TasksRun.zip', sep='')
+download.file(urlRuns,archiveRuns)
+unzip(archiveRuns)
+taskruns <- paste('json/', project, '_task_run.json', sep='')
+renameRuns <-paste(project, '_task_run.json', sep='')   
+
+file.rename(renameRuns, taskruns)
+which(lapply(readLines(taskruns), function(x) tryCatch({jsonlite::fromJSON(x); 1}, error=function(e) 0)) == 0)
+trTr <- fromJSON(paste(readLines(taskruns), collapse=""))
+
+# Re-arrange slightly and drop some columns
+trTr <- cbind(trTr$info,trTr$user_id,trTr$task_id)
+names(trTr)[length(names(trTr))] <- "taskID"
+names(trTr)[length(names(trTr))-1] <- "userID"
+
+# Sort by user ID then by task ID
+trTr <- trTr[with(trTr, order(taskID, userID)), ]
+
+# Add user credit
+tsks <- unique(as.character(trTr$taskID))
+credits <- data.frame(taskID=character(length(tsks)),inputBy=character(length(tsks)), stringsAsFactors = FALSE) #blank df to fill
+
+for (a in 1:length(tsks)){
+  atask <- trTr[trTr$taskID == tsks[a],]
+  contribs <- sort(unique(as.numeric(as.character(atask$userID))))
+  contribsNm <- users[users$id %in% contribs,]
+  credits$taskID[a] <- tsks[a]
+  credits$inputBy[a] <- paste(as.character(contribsNm$fullname), collapse="; ")
+}
+
+# Merge task summaries with image URL and user credit data.
+credurl <- merge(credits, trT, by="taskID")
+trTr <- merge(trTr,credurl, by="taskID")
+
+# Add two skipped lines between each unique index cards (i.e. between task sets).
+trTr1 <- trTr[which(is.na(trTr$taskID)), ] #blank df to fill
+newrow <- rep(NA,ncol(trTr))
+
+for (a in 1:length(tsks)){
+  atask <- trTr[trTr$taskID == tsks[a],]
+  trTr1 <- rbind(trTr1,atask,newrow,newrow)
+}
+
+# Annotations column not really needed, so dropped
+#keeps <- c("taskID","userID","day1date", "day1Text", "imageTitle", "objectNumber", 'negativeNumber', 'distribution', 'otherNotes', 'inputBy', 'comments', 'imageURL')
+#trTr2 <- trTr1[,keeps,drop=FALSE]
+
+# Finally reorder the columns fo the data to something easier to refer to:
+preforder <- c("taskID","userID","day1date", "day1text", "day2date", "day2text", "day3date", "day3text", "day4date", "day4text", "comments", "imageURL", "inputBy")
+trTr2 <- trTr2[ ,preforder]
+
+# Export as csv file
+csvname <- paste('csv/', project, '.csv', sep='')
+write.csv(trTr2, file=csvname, row.names=FALSE, na="")
