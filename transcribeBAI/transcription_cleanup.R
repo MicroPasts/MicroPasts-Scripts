5fe667be1a6fb1dc1a08a0d8b535d503dde614ff
Daniel Pett <dpett@britishmuseum.org>
2015-12-18 17:02:22 +0000

    Updated script for BAI
    This copes with zip file downloads of task data.

--- transcribeBAI/transcription_cleanup.R
+++ transcribeBAI/transcription_cleanup.R
@@ -1,29 +1,75 @@
 
 # Set working directory (for example as below)
-setwd("~/Documents/research/micropasts/analysis") #MacOSX
+setwd("~/Documents/research/micropasts/analysis/BAI/") #MacOSX
 #setwd("C:\\micropasts\\analysis") #Windows
 #setwd("micropasts/analysis") #Linux
 
 # Add necessary library
 library(jsonlite)
 
+# Create CSV directory if does not exist
+if (!file.exists('csv')){
+  dir.create('csv')
+}
+
+# Create archives directory if does not exist
+if (!file.exists('archives')){
+  dir.create('archives')
+}
+
+# Create JSON folder 
+if (!file.exists('json')){
+  dir.create('json')
+}
+
 # Load user data
 #http://crowdsourced.micropasts.org/admin/users/export?format=csv (when logged in as admin)
-users <- read.csv("csv/users.csv", header=TRUE)
+users <- read.csv("csv/all_users.csv", header=TRUE)
 users <- users[,c("id","fullname","name")]
 
-# Import tasks from json
-trTURL <- "http://crowdsourced.micropasts.org/app/arretonHoard/tasks/export?type=task&format=json"
-trT <- fromJSON(paste(readLines(trTURL), collapse=""))
+# Set the project name
+project <- ''
+
+# Set the base url of the application
+baseUrl <- 'http://crowdsourced.micropasts.org/project/'
+# Set the task runs api path
+tasks <- '/tasks/export?type=task&format=json'
+# Form the export url
+url <- paste(baseUrl,project, tasks, sep='')
+print(url)
+archives <- paste('archives/',project,'Tasks.zip', sep='')
+print(archives)
+# Import tasks from json, this method has changed due to coding changes by SciFabric to their code
+download.file(url,archives)
+unzip(archives)
+taskPath <- paste('json/', project, '.json', sep='')
+rename <- paste(project, '_task.json', sep='')
+file.rename(rename, taskPath)
+
+# Read json files
+which(lapply(readLines(taskPath), function(x) tryCatch({jsonlite::fromJSON(x); 1}, error=function(e) 0)) == 0)
+trT <- fromJSON(paste(readLines(taskPath), collapse=""))
 trT <- cbind(trT$id,trT$info)
 trTfull <- trT
+
 # extract just task id and image URL
 trT <- trT[,c(1,4)]
 names(trT) <- c("taskID","imageURL")
 
 # Import task runs from json
-trTrURL <- "http://crowdsourced.micropasts.org/app/arretonHoard/tasks/export?type=task_run&format=json"
-trTr <- fromJSON(paste(readLines(trTrURL), collapse=""))
+taskruns <- '/tasks/export?type=task_run&format=json'
+urlRuns <- paste(baseUrl,project, taskruns, sep='')
+print(urlRuns)
+archiveRuns <-paste('archives/', project, 'TasksRun.zip', sep='')
+download.file(urlRuns,archiveRuns)
+unzip(archiveRuns)
+taskruns <- paste('json/', project, '_task_run.json', sep='')
+renameRuns <-paste(project, '_task_run.json', sep='')   
+
+file.rename(renameRuns, taskruns)
+which(lapply(readLines(taskruns), function(x) tryCatch({jsonlite::fromJSON(x); 1}, error=function(e) 0)) == 0)
+trTr <- fromJSON(paste(readLines(taskruns), collapse=""))
+
 # Re-arrange slightly and drop some columns
 trTr <- cbind(trTr$info,trTr$user_id,trTr$task_id)
 names(trTr)[length(names(trTr))] <- "taskID"
@@ -58,11 +104,11 @@ tsks <- unique(as.character(trTr$taskID))
 credits <- data.frame(taskID=character(length(tsks)),inputBy=character(length(tsks)), stringsAsFactors = FALSE) #blank df to fill
 
 for (a in 1:length(tsks)){
-    atask <- trTr[trTr$taskID == tsks[a],]
-    contribs <- sort(unique(as.numeric(as.character(atask$userID))))
-    contribsNm <- users[users$id %in% contribs,]
-    credits$taskID[a] <- tsks[a]
-    credits$inputBy[a] <- paste(as.character(contribsNm$fullname), collapse="; ")
+  atask <- trTr[trTr$taskID == tsks[a],]
+  contribs <- sort(unique(as.numeric(as.character(atask$userID))))
+  contribsNm <- users[users$id %in% contribs,]
+  credits$taskID[a] <- tsks[a]
+  credits$inputBy[a] <- paste(as.character(contribsNm$fullname), collapse="; ")
 }
 
 # Merge task summaries with image URL and user credit data.
@@ -73,14 +119,22 @@ trTr <- merge(trTr,credurl, by="taskID")
 trTr1 <- trTr[which(is.na(trTr$taskID)), ] #blank df to fill
 newrow <- rep(NA,ncol(trTr))
 for (a in 1:length(tsks)){
-    atask <- trTr[trTr$taskID == tsks[a],]
-    trTr1 <- rbind(trTr1,atask,newrow,newrow,newrow)
+  atask <- trTr[trTr$taskID == tsks[a],]
+  trTr1 <- rbind(trTr1,atask,newrow,newrow,newrow)
 }
-
+print(colnames(trTr1))
 # Finally reorder the columns fo the data to something easier to refer to:
-preforder <- c("taskID","userID","objectType","rightCorner","collection","site","toSearch","Lon","Lat","optionsAccuracy","gridRef","dateDiscoveryDay","dateDiscoveryMonth","dateDiscoveryYear","length","width","edge","weight","patina","surface","thickness","other","composition","associations","description","publications","remarks","inputBy","imageURL")
+preforder <- c(
+  "taskID","userID","objectType","rightCorner","collection","site",
+  "toSearch","lat","lon","gridRef","dateDiscoveryDay",
+  "dateDiscoveryMonth","dateDiscoveryYear","length","width","edge",
+  "weight","patina","surface","thickness","other","composition",
+  "associations","description","publications","remarks","inputBy",
+  "imageURL"
+)
 trTr1 <- trTr1[ ,preforder]
 
 
 # Export as csv file
-write.csv(trTr1, file="trTr.csv",row.names=FALSE, na="")
+csvname <- paste('csv/', project, '.csv', sep='')
+write.csv(trTr1, file=csvname,row.names=FALSE, na="")
